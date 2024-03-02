d6e7a7f47bba7b609994d45104d6fc44ac852a07
faridhaavasi <faridhavasi777@gmail.com>
2024-03-03 01:44:56 +0330

    firdt view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+
+class HomePage(View):
+    template_name = 'home/index.html'
+    def get(self, request):
+        return render(request, template_name=self.template_name)
+
+
