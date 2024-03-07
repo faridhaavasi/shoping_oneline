59efcc953c9f4a80f9e288368ac05f08fdd1b30b
faridhaavasi <faridhavasi777@gmail.com>
2024-03-08 00:11:33 +0330

    add list of products vire

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home_page'),
+    path('products/', views.ListOfProductsView.as_view(), name='list_of_products'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Product
 # Create your views here.
 
 class HomePage(View):
@@ -7,4 +8,10 @@ class HomePage(View):
     def get(self, request):
         return render(request, template_name=self.template_name)
 
+
+class ListOfProductsView(View):
+    template_name = 'home/list_of_products.html'
+    def get(self, request):
+        products = Product.objects.available_True_manager()
+        return render(request, self.template_name, {'products': products})
 
