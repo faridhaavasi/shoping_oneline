af64eaf6d83e2c95d0e8b11b6e972df58110f81e
faridhaavasi <faridhavasi777@gmail.com>
2024-03-07 12:38:26 +0330

    add category level one

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(unique=True, max_length=40, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.name}-{self.created_at}'
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.slug = slugify(self.name)
+        super(Category, self).save()
+
