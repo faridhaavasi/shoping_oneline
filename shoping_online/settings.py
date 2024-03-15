9504c4df07ebdcf44c094c8b6ae2de994fb9cc19
faridhaavasi <faridhavasi777@gmail.com>
2024-03-15 15:27:21 +0330

    reset

--- shoping_online/settings.py
+++ shoping_online/settings.py
@@ -131,16 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-# DEFAULT_FILE_STORAGE = "storages.backends.s3.S3Storage"
-
-# STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
-# AWS_S3_ENDPOINT_URL = ""
-# AWS_S3_ACCESS_KEY_ID = ""
-# AWS_S3_SECRET_ACCESS_KEY = ""
-# AWS_S3_STORAGE_BUCKET_NAME = ""
-
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
