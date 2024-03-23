0b01feefccfa1f43d3fc4a132398675d1b152326
faridhaavasi <faridhavasi777@gmail.com>
2024-03-23 22:27:43 +0330

    edit relatuion category

--- home/models.py
+++ home/models.py
@@ -22,7 +22,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(unique=True, max_length=50, blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    category = models.ManyToManyField(Category,  related_name='products')
     description = models.TextField()
     image = models.ImageField()
     available = models.BooleanField(default=True)
