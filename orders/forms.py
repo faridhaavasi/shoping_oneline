34975f89ff1e0762e1df58920f666304a24cab00
faridhaavasi <faridhavasi777@gmail.com>
2024-03-30 22:53:00 +0330

    add validitor in cartaddform

--- orders/forms.py
+++ orders/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class CartAddForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=9)
+
+    def clean(self):
+        quantity = self.cleaned_data['quantity']
+        if int(quantity) > 9:
+            raise forms.ValidationError('The number of products in the order should not be more than 9')
+        return quantity
