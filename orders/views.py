b7f8536b1d512d77c026286ba0891b4049fa799e
faridhaavasi <faridhavasi777@gmail.com>
2024-03-30 15:13:12 +0330

    cart view 1

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+class CartView(View):
+    template_name = 'orders/cart.html'
+    def get(self, request):
+        return render(request, self.template_name, {})
+
