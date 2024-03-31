6841c48c5e524365ebc9ea0bf37479bc29c842df
faridhaavasi <faridhavasi777@gmail.com>
2024-03-31 18:26:22 +0330

    afix bug

--- orders/cart.py
+++ orders/cart.py
@@ -16,7 +16,7 @@ class Cart:
 		products = Product.objects.filter(id__in=product_ids)
 		cart = self.cart.copy()
 		for product in products:
-			cart[str(product.id)]['product'] = product
+			cart[str(product.id)]['product'] = product.name
 
 		for item in cart.values():
 			item['total_price'] = int(item['price']) * item['quantity']
@@ -28,7 +28,7 @@ class Cart:
 	def add(self, product, quantity):
 		product_id = str(product.id)
 		if product_id not in self.cart:
-			self.cart[product_id] = {'quantity':0, 'price':str(product.price)}
+			self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
 		self.cart[product_id]['quantity'] += quantity
 		self.save()
 
--- orders/views.py
+++ orders/views.py
@@ -15,7 +15,9 @@ class CartAddView(View):
 	def post(self, request, product_id):
 		cart = Cart(request)
 		product = get_object_or_404(Product, id=product_id)
-		form = CartAddForm(request.POST)
+		form = CartAddForm(data=request.POST)
 		if form.is_valid():
-			cart.add(product, form.cleaned_data['quantity'])
-		return redirect('orders:cart')
+			cart.add(product, quantity=form.cleaned_data['quantity'])
+
+			return redirect('orders:cart')
+		return redirect('home:detail_product')
