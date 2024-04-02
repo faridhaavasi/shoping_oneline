1b68ee4e8c48567370be191b61c3859da8fd5b85
faridhaavasi <faridhavasi777@gmail.com>
2024-04-02 21:01:35 +0330

    a bug in system cart cart.html

--- orders/views.py
+++ orders/views.py
@@ -21,3 +21,11 @@ class CartAddView(View):
 
 			return redirect('orders:cart')
 		return redirect('home:detail_product')
+
+class CartRemoveView(View):
+	def get(self, request, product_id):
+		cart = Cart(request)
+		product = get_object_or_404(Product, id=product_id)
+		cart.remove(product)
+		return redirect('orders:cart')
+
