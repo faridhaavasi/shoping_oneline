b92f1b0eebe55ebe7b443722b2f8851a2a1acc63
faridhaavasi <faridhavasi777@gmail.com>
2024-04-03 21:35:52 +0330

    add checkout

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name = 'orders'
 urlpatterns = [
+    path('create_order', views.CreateOrderView.as_view(), name='create_order'),
+    path('detail_order/<int:order_id>', views.OrderDetailView.as_view(), name='order_detail'),
+
     path('cart', views.CartView.as_view(), name='cart'),
     path('cart/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
     path('cart/remove/<str:product_name>/', views.CartRemoveView.as_view(), name='cart_remove'),
